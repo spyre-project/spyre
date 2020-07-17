aaaa5662dac86d8e391caf0d33580b83dacb1b01
Hilko Bengen <bengen@hilluzination.de>
2020-07-17 02:26:25 +0200

    Fix typo in stamp filename

--- 3rdparty.mk
+++ 3rdparty.mk
@@ -207,7 +207,7 @@ $(foreach pkg,$(3rdparty_TARGETS),\
 		$(foreach pkg,$(3rdparty_TARGETS),\
 			$(foreach arch,$($(pkg)_ARCHS),\
 				_3rdparty/build/$(arch)/$(pkg)-$($(pkg)_VERSION)/.build-stamp))
-	touch 3rdparty-all-stamp
+	touch 3rdparty-all.stamp
 
 .PHONY: 3rdparty-save-artifacts 3rdparty-restore-artifacts
 
