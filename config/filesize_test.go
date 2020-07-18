0fadc884a3546af0410f547ef953ad46902fff12
Hilko Bengen <bengen@hilluzination.de>
2020-07-18 17:37:20 +0200

    fix filesize-to-string conversion

---
+++ config/filesize_test.go
@@ -0,0 +1,12 @@
+package config
+
+import (
+	"testing"
+)
+
+func TestFileSize(t *testing.T) {
+	fs := fileSize(33554432)
+	if got := fs.String(); got != "32.0MB" {
+		t.Errorf("Expected 32M, got %s", got)
+	}
+}
