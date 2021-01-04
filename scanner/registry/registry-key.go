631b8ee6a96356008bc51596a8ce5bb02a8ddc86
lprat <lionel.prat9@gmail.com>
2021-01-04 19:24:51 +0100

    fix

--- scanner/registry/registry-key.go
+++ scanner/registry/registry-key.go
@@ -9,12 +9,11 @@ import (
 
 	"io/ioutil"
 
+	"github.com/forensicanalysis/fslib/filesystem/systemfs"
 	"github.com/spyre-project/spyre/config"
 	"github.com/spyre-project/spyre/log"
 	"github.com/spyre-project/spyre/report"
 	"github.com/spyre-project/spyre/scanner"
-
-	"github.com/forensicanalysis/fslib/filesystem/ntfs"
 	"golang.org/x/sys/windows/registry"
 	"www.velocidex.com/golang/regparser"
 
@@ -110,7 +109,7 @@ func keyCheck(key string, name string, valuex string, typex int) bool {
 			if _, err := os.Stat(val + "\\" + f.Name() + "\\NTUSER.dat"); err == nil {
 				log.Noticef("Open registre hive: %s", val+"\\"+f.Name()+"\\NTUSER.dat")
 				//fr, err := os.OpenFile(val+"\\"+f.Name()+"\\NTUSER.dat", os.O_RDONLY, 0600)
-				fr, err := ntfs.NTFSOpen(val + "\\" + f.Name() + "\\NTUSER.dat")
+				fr, _, err := systemfs.NTFSOpen(val + "\\" + f.Name() + "\\NTUSER.dat")
 				if err != nil {
 					log.Noticef("Error open base NTUSER: %s -- %s", val+"\\"+f.Name()+"\\NTUSER.dat", err)
 					continue
