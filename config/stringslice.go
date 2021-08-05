spyre-project/spyre

commit 840d13540e0cf26a85b0f6ac22cd1710274ffc62

Add UnmarshalText for custom config types

config/stringslice.go
@@ -17,6 +17,10 @@ func (s *StringSlice) Set(val string) (err error) {
 	return
 }
 
+func (s *StringSlice) UnmarshalText(text []byte) error {
+	s.Set(string(text))
+}
+
 func (s *StringSlice) String() string {
 	b := &bytes.Buffer{}
 	w := csv.NewWriter(b)

config/filesize.go
@@ -29,6 +29,10 @@ func (f *fileSize) Set(val string) error {
 	return errors.New("could not parse size")
 }
 
+func (f *fileSize) UnmarshalText(text []byte) error {
+	return f.Set(string(text))
+}
+
 func (f *fileSize) String() string {
 	if *f <= 0 {
 		return "none"

commit 01428d9f6c54c9f13ec815ad0c9667527a7e2cc0

Fix stringSlice.UnmarshalText build error
Breakage was introduced in 840d13540e0cf26a85b0f6ac22cd1710274ffc62

config/stringslice.go
@@ -18,7 +18,7 @@ func (s *StringSlice) Set(val string) (err error) {
 }
 
 func (s *StringSlice) UnmarshalText(text []byte) error {
-	s.Set(string(text))
+	return s.Set(string(text))
 }
 
 func (s *StringSlice) String() string {