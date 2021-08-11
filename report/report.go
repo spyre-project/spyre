spyre-project/spyre

commit 70f3fd3d523fde7862495fbaad41bc1a72ed2ac8

Fix log output, example config for variable-based log file name

report/report.go
@@ -6,19 +6,23 @@ import (
 
 	"github.com/mitchellh/go-ps"
 	"github.com/spf13/afero"
+
+	"strings"
 )
 
 var targets []target
 
 func Init() error {
+	var outfiles []string
 	for _, spec := range config.Global.ReportTargets {
 		tgt, err := mkTarget(spec)
 		if err != nil {
 			return err
 		}
 		targets = append(targets, tgt)
+		outfiles = append(outfiles, tgt.path)
 	}
-	log.Noticef("Writing report to %s", config.Global.ReportTargets)
+	log.Noticef("Writing report to %s", strings.Join(outfiles, ", "))
 	return nil
 }
 

report/target.go
@@ -24,6 +24,7 @@ type formatter interface {
 type target struct {
 	writer io.WriteCloser
 	formatter
+	path string
 }
 
 func expand(s string) string {
@@ -59,6 +60,7 @@ func mkTarget(spec string) (target, error) {
 			switch {
 			case u.Scheme == "file":
 				t.writer = &fileWriter{path: u.Path}
+				t.path = u.Path
 			default:
 				return target{}, fmt.Errorf("unrecognized scheme '%s'", u.Scheme)
 			}

example-spyre.yaml
@@ -1,4 +1,5 @@
 ## Global options
+# default: spyre_$hostname_$datetime.log
 report: spyre.log
 # hostname: some-random-host
 # max-file-size: 32M

commit 7ed04f4abeabb04b52f10f97ee0d9c4f796fa88d

Find a better way to output logfile names
This fixes test breakage introduced in 70f3fd3d523fde7862495fbaad41bc1a72ed2ac8

report/report.go
@@ -20,7 +20,9 @@ func Init() error {
 			return err
 		}
 		targets = append(targets, tgt)
-		outfiles = append(outfiles, tgt.path)
+		if fw, ok := tgt.writer.(*fileWriter); ok {
+			outfiles = append(outfiles, fw.path)
+		}
 	}
 	log.Noticef("Writing report to %s", strings.Join(outfiles, ", "))
 	return nil

report/target.go
@@ -24,7 +24,6 @@ type formatter interface {
 type target struct {
 	writer io.WriteCloser
 	formatter
-	path string
 }
 
 func expand(s string) string {
@@ -60,7 +59,6 @@ func mkTarget(spec string) (target, error) {
 			switch {
 			case u.Scheme == "file":
 				t.writer = &fileWriter{path: u.Path}
-				t.path = u.Path
 			default:
 				return target{}, fmt.Errorf("unrecognized scheme '%s'", u.Scheme)
 			}